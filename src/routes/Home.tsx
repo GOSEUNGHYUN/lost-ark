e07d412ac0146882d0773161fc5c6113ceff04ea
GOSEUNGHYUN <156834770+GOSEUNGHYUN@users.noreply.github.com>
2024-11-30 20:03:05 +0900

    Home 화면 수정
    배경화면 적용
    input, button 스타일 적용

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,5 +1,51 @@
 import { ChangeEvent, FormEvent, useState } from "react";
 import { useHistory } from "react-router-dom";
+import styled from "styled-components";
+
+const HomeBackground = styled.div`
+  height: 100vh;
+  width: 100vw;
+  background-image: url("/images/background.jpg");
+  background-size: 100% 100%;
+  background-position: center;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const FormContainer = styled.form`
+  text-align: center;
+
+  display: flex;
+  align-items: center;
+  gap: 5px;
+`;
+
+const InputField = styled.input`
+  width: 250px;
+  padding: 10px;
+  margin: 10px 0;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  font-size: 16px;
+
+  outline: none;
+`;
+
+const SearchButton = styled.button`
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+  background-color: #007bff;
+  color: white;
+  font-size: 16px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #0056b3;
+  }
+`;
 
 function Home() {
   const history = useHistory();
@@ -19,14 +65,16 @@ function Home() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        onChange={handleChange}
-        type="text"
-        placeholder="닉네임을 입력해주세요"
-      />
-      <button>검색</button>
-    </form>
+    <HomeBackground>
+      <FormContainer onSubmit={handleSubmit}>
+        <InputField
+          onChange={handleChange}
+          type="text"
+          placeholder="닉네임을 입력해주세요"
+        />
+        <SearchButton>검색</SearchButton>
+      </FormContainer>
+    </HomeBackground>
   );
 }
 export default Home;
